bf6bfb88657d0a0601572be268cb615f7e03ca34
izales <zales.ilias@gmail.com>
2022-02-23 15:23:03 +0100

    build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/macpro/Documents/GitHub/Portfolio/.cache/dev-404-page.js")),
   "component---src-pages-404-tsx": preferDefault(require("/Users/macpro/Documents/GitHub/Portfolio/src/pages/404.tsx")),
   "component---src-pages-index-tsx": preferDefault(require("/Users/macpro/Documents/GitHub/Portfolio/src/pages/index.tsx")),
   "component---src-pages-projects-tsx": preferDefault(require("/Users/macpro/Documents/GitHub/Portfolio/src/pages/projects.tsx")),
